7798390bb150cda3fae89ce3f2df7f3877d55d66
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-17 10:55:06 +0200

    created end point for one story

--- routers/stories.js
+++ routers/stories.js
@@ -1,17 +1,41 @@
 const { Router } = require("express");
 const Story = require("../models").story;
 const Paragraph = require('../models/').paragraph
+const User = require('../models').user
 
 const router = new Router();
 
 router.get("/", async (req, res) => {
   try {
-    const allStories = await Story.findAll()
+    const allStories = await Story.findAll({
+      include: [User]
+    })
     res.status(200).json(allStories);
   }
   catch (e) {
     console.log('error: ', e);
   }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const oneStory = await Story.findOne({
+      where: { id: req.params.id },
+      include: [Paragraph, User]
+    })
+    res.status(200).json(oneStory);
+  }
+  catch (e) {
+    console.log('error: ', e)
+  }
+})
+
+// router.get('/:id', async (req, res) => {
+//   const artwork = await Artwork.findOne({
+//     where: { id: req.params.id},
+//     include: [Bid]
+//   });
+//   res.status(200).json(artwork)
+// });
 
 module.exports = router;
